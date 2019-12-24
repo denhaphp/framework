381688ed900b4f482c44b13c05fd5754a49ba084
四月 <dancefun@163.com>
2019-12-24 16:41:36 +0800

    增加whoops

--- src/Exception.php
+++ src/Exception.php
@@ -2,9 +2,15 @@
 //------------------------
 //· 错误调试信息
 //---------------------
+
+declare (strict_types = 1);
+
 namespace denha;
 
-use denha\Config;
+use denha\HttpResource;
+use denha\Log;
+use \Whoops\Handler\JsonResponseHandler;
+use \Whoops\Handler\PlainTextHandler;
 use \Whoops\Handler\PrettyPageHandler;
 use \Whoops\Run as ErrorRun;
 
@@ -16,11 +22,58 @@ class Exception extends \Exception
      */
     protected $data = [];
 
-    public static function run()
+    /**
+     * [run description]
+     * @date   2019-12-23T09:45:04+0800
+     * @author ChenMingjiang
+     * @param  [type]                   $HttpResource [description]
+     * @param  array                    $options      [description]
+     * @return [type]                   [description]
+     */
+    public static function run(HttpResource $httpResource, $config)
     {
+
         $whoops = new ErrorRun;
-        $whoops->prependHandler(new PrettyPageHandler);
+
+        // 隐藏错误信息
+        if ($config['debug'] == false) {
+
+            header("http/1.1 404 not found");
+            header("status: 404 not found");
+
+            if ($httpResource->getMethod() == 'CLI') {
+                $whoops->prependHandler(function () {echo 'denha have a error so kill';});
+            } elseif ($httpResource->isAjax()) {
+                $whoops->prependHandler(function () {echo '{"errord":"denha have a error so kill"}';});
+            } else {
+                $whoops->prependHandler(function () use ($config) {return include $config['error']['tpl'];});
+            }
+
+            // 保存错误日志
+            if ($config['error']['save_log']) {
+                $whoops->pushHandler(function ($exception, $inspector, $run) {
+                    Log::warning($exception);
+                });
+            }
+        }
+        // cli模式
+        elseif ($httpResource->getMethod() == 'CLI') {
+            $whoops->prependHandler(new PlainTextHandler);
+        }
+        //  ajax模式
+        elseif ($httpResource->isAjax()) {
+            $whoops->prependHandler(new JsonResponseHandler);
+        }
+        // web页面模式
+        else {
+            $handler = new PrettyPageHandler;
+            $handler->setPageTitle("denha have errors");
+            // $handler->setEditor(function ($file, $line) {error_log($file . $line, 3, DATA_RUN_PATH . '1.log');});
+            $whoops->prependHandler($handler);
+        }
+
         $whoops->register();
+
     }
 
     /**
