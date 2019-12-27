d7e6367e6a202e3191934fe2a34b72b582327524
四月 <dancefun@163.com>
2019-12-27 16:51:29 +0800

    代码删简

--- src/Route.php
+++ src/Route.php
@@ -24,16 +24,13 @@ class Route
     public static $id         = 0;
     public static $regularUrl = []; // 路由规则数组
 
-    public function __construct(){
-        
-    }
 
     // 执行主体
     public static function main()
     {
         self::$config          = Config::get('route');
         self::$thisRule['uri'] = self::$uri = self::parseUri();
-
+        
         // 检查规则路由
         if (self::$config['open_route']) {
             // 加载路由规则文件
@@ -246,9 +243,6 @@ class Route
     // 解析路由
     private static function parseUri()
     {
-        //去除urldecode转码 转码会导致get参数 带/解析错误
-        // $uri = urldecode($_SERVER['REQUEST_URI']);
-
         if (isset($_SERVER['REQUEST_URI'])) {
             $uri = $_SERVER['REQUEST_URI'];
         } elseif (isset($_SERVER['argv'][1])) {
